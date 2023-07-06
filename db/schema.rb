8962df65356ddbde4a00244c3fcada145b7a76df
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-07-06 14:06:20 +0530

    dwdw

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_080154) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_083428) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -79,6 +79,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_080154) do
     t.string "attachment"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.string "resume"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -95,4 +105,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_080154) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "job_applications", "jobs"
   add_foreign_key "job_applications", "users"
+  add_foreign_key "profiles", "users"
 end
