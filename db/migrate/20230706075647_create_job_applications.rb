a2009de1038af33bafb71d701d0a51062db4cb92
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-07-06 13:33:44 +0530

    ijiiiiiii

--- db/migrate/20230706075647_create_job_applications.rb
+++
@@ -1,8 +0,0 @@
-class CreateJobApplications < ActiveRecord::Migration[7.0]
-  def change
-    create_table :job_applications do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_054132) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_080154) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,6 +51,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_054132) do
     t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
   end
 
+  create_table "job_applications", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "resume"
+    t.integer "job_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_job_applications_on_job_id"
+    t.index ["user_id"], name: "index_job_applications_on_user_id"
+  end
+
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -81,4 +93,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_054132) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "job_applications", "jobs"
+  add_foreign_key "job_applications", "users"
 end
