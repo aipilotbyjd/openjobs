11c5925beb5d9172d6e21b90904d3635755290cd
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-07-06 12:00:31 +0530

    dswd

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: %i[ show edit update destroy ]
+  before_action :authenticate_company!, only: %i[ new create edit update destroy ]
 
   # GET /jobs or /jobs.json
   def index
