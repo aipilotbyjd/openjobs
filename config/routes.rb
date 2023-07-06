30f77ce59485af3e7554ca927a00058ff4facc50
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-07-06 11:17:24 +0530

    mooo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :companies
+  devise_for :users
   resources :jobs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
